4af16c4c93b78c5a5c069d3a451e70e4eca67822
Adriein <adria.claret@gmail.com>
2024-03-23 18:34:48 +0100

    clean hoap parser

--- src/HoapParser.ts
+++ src/HoapParser.ts
@@ -2,10 +2,6 @@ import {XMLParser} from "fast-xml-parser";
 
 export default class HoapParser {
     public async parse(rawXML: string): Promise<void> {
-        const parser: XMLParser = new XMLParser();
-
-        const result = parser.parse(rawXML);
-
-        console.log(result['soap:Envelope']['soap:Body']);
+        console.log(rawXML);
     }
 }
