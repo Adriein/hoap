51cd7395f8478c239322c1f316e4b6065a7d9dc1
Adriein <adria.claret@gmail.com>
2025-01-01 10:42:32 +0100

    feat(HO-6): wrap parse function with a promise

--- src/Parser.ts
+++ src/Parser.ts
@@ -27,74 +27,101 @@ export class Parser {
         this.WATCHED_XML_TAG_TREE = InstructionTreeBuilder.fromHoapConfigJson(config.configFile);
     }
 
-    public parse(): void {
-        if (!this.config.path) {
-            throw ParserConfigError.noPathProvided();
-        }
+    public parse(): Promise<void> {
+        return new Promise((resolve: (json: any) => void, reject: (error: Error) => void) => {
+            if (!this.config.path) {
+                throw ParserConfigError.noPathProvided();
+            }
 
-        const stream: ReadStream = fs.createReadStream(this.config.path);
+            const stream: ReadStream = fs.createReadStream(this.config.path);
 
-        let bufferLeftover: Buffer<ArrayBuffer> = Buffer.alloc(0);
+            let bufferLeftover: Buffer<ArrayBuffer> = Buffer.alloc(0);
 
-        const resultTree: ResultTreeNode = ResultTreeNode.init();
+            const resultTree: ResultTreeNode = ResultTreeNode.init();
 
-        this.RESULT_TREE_HASH_MAP.set("root", [resultTree]);
+            this.RESULT_TREE_HASH_MAP.set("root", [resultTree]);
 
-        let globalIndexPosition: number = 0;
+            let globalIndexPosition: number = 0;
 
-        stream.on("data", (chunk: Buffer<ArrayBuffer>): void => {
-            const chunkCombinedWithLeftover: Buffer<ArrayBuffer> = Buffer.concat([bufferLeftover, chunk]);
+            stream.on("data", (chunk: Buffer<ArrayBuffer>): void => {
+                const chunkCombinedWithLeftover: Buffer<ArrayBuffer> = Buffer.concat([bufferLeftover, chunk]);
 
-            let securityBytesBuffer: number = 0;
+                let securityBytesBuffer: number = 0;
 
-            XmlTreeTraverser.dfs(this.WATCHED_XML_TAG_TREE, (node: XmlTreeNode, path: string): void => {
-                const {original, type, open, close}: RawBinaryXmlTagPair = node.data;
+                XmlTreeTraverser.dfs(this.WATCHED_XML_TAG_TREE, (node: XmlTreeNode, path: string): void => {
+                    const {original, type, open, close}: RawBinaryXmlTagPair = node.data;
 
-                let observedChunk: Buffer<ArrayBuffer> = chunkCombinedWithLeftover;
-                let subtractedChunkBytes: number = 0;
+                    let observedChunk: Buffer<ArrayBuffer> = chunkCombinedWithLeftover;
+                    let subtractedChunkBytes: number = 0;
 
-                let openTagIndex: number = 0;
-                let closeTagIndex: number = 0;
+                    let openTagIndex: number = 0;
+                    let closeTagIndex: number = 0;
 
-                // The parser try to find all occurrences of the current tag
-                while(openTagIndex !== -1) {
-                    openTagIndex = observedChunk.indexOf(open);
-                    closeTagIndex = observedChunk.indexOf(close);
+                    // The parser try to find all occurrences of the current tag
+                    while(openTagIndex !== -1) {
+                        openTagIndex = observedChunk.indexOf(open);
+                        closeTagIndex = observedChunk.indexOf(close);
 
-                    // Open and close tag found in the observable chunk
-                    if (openTagIndex !== -1 && closeTagIndex !== -1) {
-                        // The closing tag index is greater than opening tag
-                        // meaning that is a closing tag from another chunk
-                        if(closeTagIndex < openTagIndex) {
-                            this.closeOpenNode(path, closeTagIndex + globalIndexPosition);
+                        // Open and close tag found in the observable chunk
+                        if (openTagIndex !== -1 && closeTagIndex !== -1) {
+                            // The closing tag index is greater than opening tag
+                            // meaning that is a closing tag from another chunk
+                            if(closeTagIndex < openTagIndex) {
+                                this.closeOpenNode(path, closeTagIndex + globalIndexPosition);
 
-                            const currentObservedChunkBytes: number = observedChunk.byteLength;
+                                const currentObservedChunkBytes: number = observedChunk.byteLength;
 
-                            observedChunk = observedChunk.subarray(
-                                closeTagIndex + close.byteLength,
-                                chunkCombinedWithLeftover.byteLength
-                            );
+                                observedChunk = observedChunk.subarray(
+                                    closeTagIndex + close.byteLength,
+                                    chunkCombinedWithLeftover.byteLength
+                                );
 
-                            subtractedChunkBytes = this.calculateSubtractedChunkBytes(
-                                subtractedChunkBytes,
-                                currentObservedChunkBytes,
-                                observedChunk.byteLength
-                            );
+                                subtractedChunkBytes = this.calculateSubtractedChunkBytes(
+                                    subtractedChunkBytes,
+                                    currentObservedChunkBytes,
+                                    observedChunk.byteLength
+                                );
 
-                            continue;
-                        }
+                                continue;
+                            }
 
-                        if(type !== XML_NODE_TYPE) {
-                            const rawBinaryValue: Buffer<ArrayBuffer> = observedChunk.subarray(
-                                openTagIndex + open.byteLength,
-                                closeTagIndex
-                            );
+                            if(type !== XML_NODE_TYPE) {
+                                const rawBinaryValue: Buffer<ArrayBuffer> = observedChunk.subarray(
+                                    openTagIndex + open.byteLength,
+                                    closeTagIndex
+                                );
+
+                                const result: ResultTreeNode = this.createResultNode(
+                                    original,
+                                    openTagIndex + globalIndexPosition + subtractedChunkBytes,
+                                    closeTagIndex + globalIndexPosition + subtractedChunkBytes,
+                                    rawBinaryValue.toString(UTF_8_ENCODING)
+                                )
+
+                                this.registerNewNode(path, result);
+
+                                this.append(path, result);
+
+                                const currentObservedChunkBytes: number = observedChunk.byteLength;
+
+                                observedChunk = observedChunk.subarray(
+                                    closeTagIndex + close.byteLength,
+                                    chunkCombinedWithLeftover.byteLength
+                                );
+
+                                subtractedChunkBytes = this.calculateSubtractedChunkBytes(
+                                    subtractedChunkBytes,
+                                    currentObservedChunkBytes,
+                                    observedChunk.byteLength
+                                );
+
+                                continue;
+                            }
 
                             const result: ResultTreeNode = this.createResultNode(
                                 original,
                                 openTagIndex + globalIndexPosition + subtractedChunkBytes,
-                                closeTagIndex + globalIndexPosition + subtractedChunkBytes,
-                                rawBinaryValue.toString(UTF_8_ENCODING)
+                                closeTagIndex + globalIndexPosition + subtractedChunkBytes
                             )
 
                             this.registerNewNode(path, result);
@@ -117,75 +144,58 @@ export class Parser {
                             continue;
                         }
 
-                        const result: ResultTreeNode = this.createResultNode(
-                            original,
-                            openTagIndex + globalIndexPosition + subtractedChunkBytes,
-                            closeTagIndex + globalIndexPosition + subtractedChunkBytes
-                        )
+                        // Only open tag found in the observed chunk
+                        if (openTagIndex !== -1) {
+                            if(type !== XML_NODE_TYPE) {
+                                securityBytesBuffer = chunk.byteLength - (openTagIndex + subtractedChunkBytes);
 
-                        this.registerNewNode(path, result);
+                                continue;
+                            }
 
-                        this.append(path, result);
+                            const result: ResultTreeNode = this.createResultNode(
+                                original,
+                                openTagIndex + globalIndexPosition + subtractedChunkBytes,
+                                closeTagIndex === -1 ? -1 : closeTagIndex + globalIndexPosition + subtractedChunkBytes
+                            )
 
-                        const currentObservedChunkBytes: number = observedChunk.byteLength;
+                            this.registerNewNode(path, result);
 
-                        observedChunk = observedChunk.subarray(
-                            closeTagIndex + close.byteLength,
-                            chunkCombinedWithLeftover.byteLength
-                        );
+                            this.append(path, result);
 
-                        subtractedChunkBytes = this.calculateSubtractedChunkBytes(
-                            subtractedChunkBytes,
-                            currentObservedChunkBytes,
-                            observedChunk.byteLength
-                        );
+                            const currentObservedChunkBytes: number = observedChunk.byteLength;
 
-                        continue;
-                    }
+                            observedChunk = observedChunk.subarray(
+                                openTagIndex + open.byteLength,
+                                chunkCombinedWithLeftover.byteLength
+                            );
 
-                    // Only open tag found in the observed chunk
-                    if (openTagIndex !== -1) {
-                        if(type !== XML_NODE_TYPE) {
-                            securityBytesBuffer = chunk.byteLength - (openTagIndex + subtractedChunkBytes);
-
-                            continue;
+                            subtractedChunkBytes = this.calculateSubtractedChunkBytes(
+                                subtractedChunkBytes,
+                                currentObservedChunkBytes,
+                                observedChunk.byteLength
+                            );
                         }
-
-                        const result: ResultTreeNode = this.createResultNode(
-                            original,
-                            openTagIndex + globalIndexPosition + subtractedChunkBytes,
-                            closeTagIndex === -1 ? -1 : closeTagIndex + globalIndexPosition + subtractedChunkBytes
-                        )
-
-                        this.registerNewNode(path, result);
-
-                        this.append(path, result);
-
-                        const currentObservedChunkBytes: number = observedChunk.byteLength;
-
-                        observedChunk = observedChunk.subarray(
-                            openTagIndex + open.byteLength,
-                            chunkCombinedWithLeftover.byteLength
-                        );
-
-                        subtractedChunkBytes = this.calculateSubtractedChunkBytes(
-                            subtractedChunkBytes,
-                            currentObservedChunkBytes,
-                            observedChunk.byteLength
-                        );
                     }
-                }
+                });
+
+                const start: number = chunkCombinedWithLeftover.byteLength - securityBytesBuffer;
+
+                bufferLeftover = chunkCombinedWithLeftover.subarray(start, chunkCombinedWithLeftover.length);
+
+                globalIndexPosition = globalIndexPosition + chunk.byteLength;
             });
 
-            const start: number = chunkCombinedWithLeftover.byteLength - securityBytesBuffer;
+            stream.on("end", (): void => {
+                console.log(resultTree);
 
-            bufferLeftover = chunkCombinedWithLeftover.subarray(start, chunkCombinedWithLeftover.length);
+                resolve({});
+            });
 
-            globalIndexPosition = globalIndexPosition + chunk.byteLength;
-        });
+            stream.on("error", (error: Error): void => {
+                console.log(error);
 
-        stream.on("end", (): void => {
-            console.log(resultTree);
+                reject(error);
+            });
         });
     }
 
