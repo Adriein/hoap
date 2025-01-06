18f64911e4fe91027fa349a1f8319d57207dbd58
Adriein <adria.claret@gmail.com>
2025-01-06 11:00:26 +0100

    feat(HO-6): cleanup

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,6 @@ import {WatchedXmlTagsJson} from "@Shared/Types";
 import {UTF_8_ENCODING} from "@Shared/Constants";
 import {HoapParser} from "@src/HoapParser";
 
-/*const hoap: ParserV0 = new ParserV0();
-
-const rawData: string = fs.readFileSync(
-    `${process.cwd()}/test/xml/Fare_MasterPricerTravelBoardSearchResponse.xml`,
-    'utf8'
-);
-hoap.parse(rawData)
-    .then(_=> {})
-    .catch(error => console.log(error));
-*/
-
 const AMADEUS_TEST_XML = `${process.cwd()}/test/xml/Fare_MasterPricerTravelBoardSearchResponse.xml`;
 const PARSER_WATCHED_XML_TAGS_CONFIG_FILE = `${process.cwd()}/src/hoap.config.json`;
 
@@ -31,8 +20,6 @@ const config: ParserConfig = ParserConfig
     .instance()
     .withFilePath(AMADEUS_TEST_XML)
     .withConfigFile(watchedXmlTagsJson)
-
-// const hoap: ParserV1 = new ParserV1(config);
 
 const hoap: HoapParser = new HoapParser(config);
 
