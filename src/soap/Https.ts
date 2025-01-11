ce2f795c13ffd26731f9c4d1ad41029af5f9dacf
Adriein <adria.claret@gmail.com>
2025-01-11 18:20:11 +0100

    feat(HO-14): wip naive soap http client impl

--- src/soap/Https.ts
+++ src/soap/Https.ts
@@ -1,1 +1,28 @@
-export class Https {}
+/*
+ * Copyright (c) 2024 Adria Claret <adria.claret@gmail.com>
+ * MIT Licensed
+ */
+
+import { request } from 'node:https';
+import { ClientRequest, IncomingMessage} from "node:http";
+
+export class Https {
+    public do(abort: (clientRequest: ClientRequest) => void): void {
+        const options = {
+            hostname: "",
+            method: 'POST',
+            headers: { 'Content-Type': 'text/xml; charset=utf-8' }
+        };
+
+        const stdClientRequest: ClientRequest = request(options, (readable: IncomingMessage): void => {
+            readable.on('data', (chunk: Buffer<ArrayBuffer>): void => {
+
+            })
+        });
+
+        abort(stdClientRequest);
+    }
+    private buildRequest(): any {
+
+    }
+}
